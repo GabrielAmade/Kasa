018d84f514acb131c04093dd6e9d02717f961075
Gabriel Amadé <104569217+GabrielAmade@users.noreply.github.com>
2023-05-18 08:48:16 +0200

    suppression fichiers inutiles

--- kasa_project/src/components/apartment_page/DescriptionContainer.jsx
+++ kasa_project/src/components/apartment_page/DescriptionContainer.jsx
@@ -1,22 +1,31 @@
-import React, {useState} from 'react'
-import "./DescriptionContainer.css"
+import React, { useState } from 'react';
+import "./DescriptionContainer.css";
 
 function DescriptionContainer(props) {
-  const [isContentVisible, setIsContentVisible] = useState(false)
+  const [isContentVisible, setIsContentVisible] = useState(false);
+  const [isChevronRotated, setIsChevronRotated] = useState(false);
 
   const showContent = () => {
-    setIsContentVisible(!isContentVisible)
-  }
+    setIsContentVisible(!isContentVisible);
+    setIsChevronRotated(!isChevronRotated);
+  };
 
   return (
     <div className='description'>
         <div className='description_title'>
             <p>{props.title}</p>
-            <span><i className="fa-solid fa-chevron-up chevron" onClick={showContent}></i></span>
+            <span>
+              <i
+                className={`fa-solid fa-chevron-up chevron ${isChevronRotated ? "rotate" : ""}`}
+                onClick={showContent}
+              ></i>
+            </span>
         </div>
-        {isContentVisible && <div className='description_content'>{props.content}</div>}
+        {isContentVisible && (
+          <div className='description_content'>{props.content}</div>
+        )}
     </div>
-  )
+  );
 }
 
-export default DescriptionContainer
+export default DescriptionContainer;
--- kasa_project/src/index.js
+++ kasa_project/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -15,11 +13,13 @@ import ErrorPage from './components/error_page/ErrorPage';
 import About from './components/about/About'
 
 const HeaderFooterLayout = ()=> {
-  return <>
-    <Navbar />
-    <Outlet />
-    <Footer />
-  </>
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
 }
 
 const router = createBrowserRouter([
@@ -51,7 +51,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
